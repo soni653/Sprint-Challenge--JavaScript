324cef4f86540ecc8b5589d707937d2cb741c903
soni653 <dave.family653@outlook.com>
2020-01-27 14:28:35 -0500

    Submission

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,7 +1,50 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
-// Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+class CuboidMaker {
+    constructor(values) {
+    this.length = values.length;
+    this.width = values.width;
+    this.height = values.height;
+ }
+ volume() {
+    return this.length * this.height * this.width 
+  }
 
-// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+ surfaceArea() {
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
+  }
+}
+  const cuboid = new CuboidMaker({
+    length: 4,
+    width: 5,
+    height: 5
+  })
+
+// Test your volume and surfaceArea methods by uncommenting the logs below:
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
+
+// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker. 
+// Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker. 
+// Test your work by logging out your volume and surface area.
+class CubeMaker extends CuboidMaker{
+    constructor(childvalues){
+        super(childvalues);
+    }
+    volume(){
+        return this.length * this.height * this.width 
+    }
+    surfaceArea(){
+        return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
+    }
+}
+ const cube = new CuboidMaker({
+    length: 6,
+    width: 7,
+    height: 6
+  })
+
+// Test your volume and surfaceArea methods by uncommenting the logs below:
+console.log(cube.volume()); // 
+console.log(cube.surfaceArea()); // 
